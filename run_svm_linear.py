ce22924045390a23aa63e58ee51352292233e93f
Chungsang Tom Lam <erinata@gmail.com>
2023-02-21 11:57:58 -0500

    implement svm with linear

--- run_svm_linear.py
+++ run_svm_linear.py
@@ -1,1 +1,25 @@
-print("hello")
+import pandas
+
+from matplotlib import pyplot
+
+from sklearn import svm
+import kfold_template
+
+dataset = pandas.read_csv("dataset/dataset_svm_1.csv")
+
+print(dataset)
+
+target = dataset.iloc[:,0].values
+
+data = dataset.iloc[:,1:3].values
+
+pyplot.scatter(data[:,0], data[:,1], c=target)
+pyplot.savefig("scatter_dataset_1.png")
+pyplot.close()
+
+machine = svm.SVC(kernel="linear")
+return_values = kfold_template.run_kfold(data, target, machine, 4, False, True, False)
+print(return_values)
+
+
+
